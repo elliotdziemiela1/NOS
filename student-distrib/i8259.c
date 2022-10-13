b77ea040d049d7c6df3e354cff3b78bc9a1a32bf
Dziemiela <edziem2@illinois.edu>
2022-10-12 22:58:55 -0500

    outputted control words to PIC, figuring out lock and delay after init

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -9,8 +9,27 @@
 uint8_t master_mask; /* IRQs 0-7  */
 uint8_t slave_mask;  /* IRQs 8-15 */
 
+
+
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
+    // +1 means data port. xff is a full mask of the PICs' IRs
+    master_mask = 0xff;
+    slave_mask = 0xff;
+
+    outb(master_mask, MASTER_8259_PORT+1); // mask all of PIC 1
+    outb(slave_mask, SLAVE_8259_PORT+1); // mask all of PIC 2
+
+    outb(ICW1, MASTER_8259_PORT); // tells PIC 1 to expect initialization words
+    outb(ICW2_MASTER, MASTER_8259_PORT+1); // tells PIC 1 where to map IR0-7 (maps to x20-x27 in IDT)
+    outb(ICW3_MASTER, MASTER_8259_PORT+1); // tells PIC 1 that has a secondary PIC on IRQ2
+    outb(ICW4, MASTER_8259_PORT+1);
+
+    outb(ICW1, SLAVE_8259_PORT); // tells PIC 2 to expect initialization words
+    outb(ICW2_SLAVE, SLAVE_8259_PORT+1); // tells PIC 2 where to map IR0-7 (maps to x28-x2f in IDT)
+    outb(ICW3_SLAVE, SLAVE_8259_PORT+1); // tells PIC 2 that it is a secondary PIC on IRQ2
+    outb(ICW4, SLAVE_8259_PORT+1);
+
 }
 
 /* Enable (unmask) the specified IRQ */
